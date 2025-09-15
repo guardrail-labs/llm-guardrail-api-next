5d2ded0ee014af58fd9e2809f8d18ef21be23a3c
DrWesMilam <45669874+WesMilam@users.noreply.github.com>
2025-09-15 17:59:26 -0400

    Make 'app' a proper package with entrypoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,7 @@
-__all__: list[str] = []
+# Make 'app' a proper package; re-export common entrypoints.
+from __future__ import annotations
+try:
+    from .main import create_app  # noqa: F401
+except Exception:
+    # Don't fail package import just because factories can't import yet.
+    pass
